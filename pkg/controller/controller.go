3530a8e391cda84792915e191707777553758891
Artur Dzmitryieu <dzmitry@ca.ibm.com>
2019-10-23 08:32:30 -0400

    Initialize golang version of operator

---
+++ pkg/controller/controller.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	prometheusv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
+	servingv1alpha1 "github.com/knative/serving/pkg/apis/serving/v1alpha1"
+	routev1 "github.com/openshift/api/route/v1"
+)
+
+// AddToManagerFuncs is a list of functions to add all Controllers to the Manager
+var AddToManagerFuncs []func(manager.Manager) error
+
+// AddToManager adds all Controllers to the Manager
+func AddToManager(m manager.Manager) error {
+	if err := routev1.AddToScheme(m.GetScheme()); err != nil {
+		return err
+	}
+
+	if err := servingv1alpha1.AddToScheme(m.GetScheme()); err != nil {
+		return err
+	}
+
+	if err := prometheusv1.AddToScheme(m.GetScheme()); err != nil {
+		return err
+	}
+	for _, f := range AddToManagerFuncs {
+		if err := f(m); err != nil {
+			return err
+		}
+	}
+	return nil
+}
