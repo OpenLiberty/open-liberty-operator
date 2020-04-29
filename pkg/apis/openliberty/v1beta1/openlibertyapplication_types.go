18740caf3220f60c04ac85056976188c6937c667
Artur Dzmitryieu <dzmitry@ca.ibm.com>
2020-04-29 11:10:19 -0400

    Update RC to v0.5.1

--- pkg/apis/openliberty/v1beta1/openlibertyapplication_types.go
+++ pkg/apis/openliberty/v1beta1/openlibertyapplication_types.go
@@ -803,7 +803,12 @@ func (cr *OpenLibertyApplication) GetLabels() map[string]string {
 
 // GetAnnotations returns set of annotations to be added to all resources
 func (cr *OpenLibertyApplication) GetAnnotations() map[string]string {
-	return cr.Annotations
+	annotations := map[string]string{}
+	for k, v := range cr.Annotations {
+		annotations[k] = v
+	}
+	delete(annotations, "kubectl.kubernetes.io/last-applied-configuration")
+	return annotations
 }
 
 // GetType returns status condition type
