ab61afe1d0b71d63425788fd750586ee25a54135
Jerry Meng <chungeng.meng@ibm.com>
2020-06-17 01:26:22 -0400

    gofmt autoscaling

--- test/e2e/openliberty_autoscaling.go
+++ test/e2e/openliberty_autoscaling.go
@@ -10,17 +10,20 @@ import (
 	"github.com/OpenLiberty/open-liberty-operator/test/util"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	e2eutil "github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+
 	autoscalingv1 "k8s.io/api/autoscaling/v1"
 	corev1 "k8s.io/api/core/v1"
 	resource "k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
+
 	k "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // OpenLibertyAutoScalingTest : More indepth testing of autoscaling
 func OpenLibertyAutoScalingTest(t *testing.T) {
-
+	// standard initialization
 	ctx, err := util.InitializeContext(t, cleanupTimeout, retryInterval)
 	if err != nil {
 		t.Fatal(err)
@@ -34,7 +37,7 @@ func OpenLibertyAutoScalingTest(t *testing.T) {
 	}
 	const name string = "example-liberty-autoscaling"
 
-	// Wait for the operator as the following configmaps won't exist until it has deployed
+	// wait for the operator as the following configmaps won't exist until it has deployed
 	err = e2eutil.WaitForOperatorDeployment(t, f.KubeClient, namespace, "open-liberty-operator", 1, retryInterval, operatorTimeout)
 	if err != nil {
 		util.FailureCleanup(t, f, namespace, err)
@@ -49,7 +52,7 @@ func OpenLibertyAutoScalingTest(t *testing.T) {
 		util.FailureCleanup(t, f, namespace, err)
 	}
 
-	// Make basic open liberty application with 1 replica
+	// make basic open liberty application with 1 replica
 	replicas := int32(1)
 	openLibertyApplication := util.MakeBasicOpenLibertyApplication(t, f, name, namespace, replicas)
 
@@ -74,7 +77,7 @@ func OpenLibertyAutoScalingTest(t *testing.T) {
 		util.FailureCleanup(t, f, namespace, err)
 	}
 
-	// Check the name field that matches
+	// check the name field that matches
 	m := map[string]string{"metadata.name": name}
 	l := fields.Set(m)
 	selec := l.AsSelector()
@@ -106,14 +109,16 @@ func getHPA(hpa *autoscalingv1.HorizontalPodAutoscalerList, t *testing.T, f *fra
 }
 
 func waitForHPA(hpa *autoscalingv1.HorizontalPodAutoscalerList, t *testing.T, minReplicas int32, maxReplicas int32, utiliz int32, f *framework.Framework, options k.ListOptions) error {
-	for counter := 0; counter < 6; counter++ {
-		time.Sleep(6000 * time.Millisecond)
+	var hpaErr error
+	wait.Poll(retryInterval, timeout, func() (done bool, err error) {
 		hpa = getHPA(hpa, t, f, options)
-		if checkValues(hpa, t, minReplicas, maxReplicas, utiliz) == nil {
-			return nil
+		hpaErr = checkValues(hpa, t, minReplicas, maxReplicas, utiliz)
+		if hpaErr != nil {
+			return false, nil
 		}
-	}
-	return checkValues(hpa, t, minReplicas, maxReplicas, utiliz)
+		return true, nil
+	})
+	return hpaErr
 }
 
 func setResources(cpu string) *corev1.ResourceRequirements {
@@ -141,11 +146,9 @@ func setAutoScale(values ...int32) *openlibertyv1beta1.OpenLibertyApplicationAut
 	}
 
 	return &openlibertyv1beta1.OpenLibertyApplicationAutoScaling{}
-
 }
 
 func checkValues(hpa *autoscalingv1.HorizontalPodAutoscalerList, t *testing.T, minReplicas int32, maxReplicas int32, utiliz int32) error {
-
 	if hpa.Items[0].Spec.MaxReplicas != maxReplicas {
 		t.Logf("Max replicas is set to: %d", hpa.Items[0].Spec.MaxReplicas)
 		return errors.New("Error: Max replicas is not correctly set")
@@ -254,7 +257,7 @@ func incorrectFieldsTest(t *testing.T, f *framework.Framework, ctx *framework.Te
 	timestamp := time.Now().UTC()
 	t.Logf("%s - Starting liberty autoscaling test...", timestamp)
 
-	// Make basic liberty application with 1 replica
+	// make basic liberty application with 1 replica
 	replicas := int32(1)
 	openLibertyApplication := util.MakeBasicOpenLibertyApplication(t, f, name, namespace, replicas)
 
@@ -270,7 +273,7 @@ func incorrectFieldsTest(t *testing.T, f *framework.Framework, ctx *framework.Te
 		util.FailureCleanup(t, f, namespace, err)
 	}
 
-	// Check the name field that matches
+	// check the name field that matches
 	m := map[string]string{"metadata.name": name}
 	l := fields.Set(m)
 	selec := l.AsSelector()
@@ -309,7 +312,7 @@ func replicasTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) 
 	timestamp := time.Now().UTC()
 	t.Logf("%s - Starting runtime autoscaling test...", timestamp)
 
-	// Make basic runtime omponent with 1 replica
+	// make basic runtime omponent with 1 replica
 	replicas := int32(2)
 	runtime := util.MakeBasicOpenLibertyApplication(t, f, name, namespace, replicas)
 
