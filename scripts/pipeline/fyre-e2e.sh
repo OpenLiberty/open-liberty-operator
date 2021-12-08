de3d1acab59d6a848692b7134c450a1a4407089d
Paul Baity <paul.baity@ibm.com>
2021-12-08 15:58:20 -0500

    Update to latest version

--- scripts/pipeline/fyre-e2e.sh
+++ scripts/pipeline/fyre-e2e.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-readonly usage="Usage: e2e.sh -u <docker-username> -p <docker-password> --cluster-url <url> --cluster-token <token> --registry-name <name> --registry-namespace <namespace> --registry-user <user> --registry-password <password>"
+readonly usage="Usage: fyre-e2e.sh -u <docker-username> -p <docker-password> --cluster-url <url> --cluster-token <token> --registry-name <name> --registry-namespace <namespace> --registry-user <user> --registry-password <password> --release <daily|release-tag> --test-tag <test-id>"
 #readonly SERVICE_ACCOUNT="travis-tests"
 readonly OC_CLIENT_VERSION="4.7.0"
+readonly CONTROLLER_MANAGER_NAME="olo-controller-manager"
 
 # setup_env: Download oc cli, log into our persistent cluster, and create a test project
 setup_env() {
@@ -13,16 +14,21 @@ setup_env() {
 
     # Start a cluster and login
     echo "****** Logging into remote cluster..."
-    oc login "${OC_URL}" -u kubeadmin -p "${OC_TOKEN}" --insecure-skip-tls-verify=true
+    oc login "${CLUSTER_URL}" -u kubeadmin -p "${CLUSTER_TOKEN}" --insecure-skip-tls-verify=true
 
     # Set variables for rest of script to use
     #readonly DEFAULT_REGISTRY=$(oc get route "${REGISTRY_NAME}" -o jsonpath="{ .spec.host }" -n "${REGISTRY_NAMESPACE}")
-    readonly TEST_NAMESPACE="olo-test-${TRAVIS_BUILD_NUMBER}"
-    readonly BUILD_IMAGE="${REGISTRY_NAME}/${REGISTRY_NAMESPACE}/olo-operator:daily"
-    readonly BUNDLE_IMAGE="${REGISTRY_NAME}/${REGISTRY_NAMESPACE}/olo-operator:bundle-daily"
+    readonly TEST_NAMESPACE="olo-test-${TEST_TAG}"
+    readonly BUILD_IMAGE="${REGISTRY_NAME}/${REGISTRY_NAMESPACE}/olo-operator:${RELEASE}"
+    readonly BUNDLE_IMAGE="${REGISTRY_NAME}/${REGISTRY_NAMESPACE}/olo-operator:bundle-${RELEASE}"
 
-    echo "****** Creating test namespace: ${TEST_NAMESPACE}"
+    echo "****** Creating test namespace: ${TEST_NAMESPACE} for release ${RELEASE}"
     oc new-project "${TEST_NAMESPACE}" || oc project "${TEST_NAMESPACE}"
+
+    ## Switch to release branch
+    if [[ "${RELEASE}" != "daily" ]]; then
+      git checkout -q "${RELEASE}"
+    fi
 
     ## Create service account for Kuttl tests
     oc apply -f config/rbac/kuttl-rbac.yaml
@@ -57,20 +63,24 @@ cleanup_env() {
 main() {
     parse_args "$@"
 
-    if [[ -z "${USER}" || -z "${PASS}" ]]; then
+    if [[ -z "${RELEASE}" ]]; then
+        echo "****** Missing release, see usage"
+    fi
+
+    if [[ -z "${DOCKER_USERNAME}" || -z "${DOCKER_PASSWORD}" ]]; then
         echo "****** Missing docker authentication information, see usage"
         echo "${usage}"
         exit 1
     fi
 
-    if [[ -z "${OC_URL}" ]] || [[ -z "${OC_TOKEN}" ]]; then
+    if [[ -z "${CLUSTER_URL}" ]] || [[ -z "${CLUSTER_TOKEN}" ]]; then
         echo "****** Missing OCP URL or token, see usage"
         echo "${usage}"
         exit 1
     fi
 
     if [[ -z "${REGISTRY_NAME}" ]] || [[ -z "${REGISTRY_NAMESPACE}" ]]; then
-        echo "****** Missing registry name or registry namespace, see usage"
+        echo "****** Missing OCP registry name or registry namespace, see usage"
         echo "${usage}"
         exit 1
     fi
@@ -81,25 +91,43 @@ main() {
         exit 1
     fi
 
+    if [[ -z "${TEST_TAG}" ]]; then
+        echo "****** Missing test tag, see usage"
+        echo "${usage}"
+        exit 1
+    fi
+
     echo "****** Setting up test environment..."
     setup_env
 
     # login to docker to avoid rate limiting during build
-    echo "${PASS}" | docker login -u "${USER}" --password-stdin
+    echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
-    echo "****** Building image..."
-    docker build -t "${BUILD_IMAGE}" .
+    #echo "****** Building image..."
+    #docker build -t "${BUILD_IMAGE}" .
 
-    echo "****** Building bundle..."
-    IMG="${BUILD_IMAGE}" BUNDLE_IMG="${BUNDLE_IMAGE}" make kustomize bundle bundle-build
+    #echo "****** Building bundle..."
+    #IMG="${BUILD_IMAGE}" BUNDLE_IMG="${BUNDLE_IMAGE}" make kustomize bundle bundle-build
 
     #echo "****** Pushing operator and operator bundle images into registry..."
     #push_images
 
+    trap "rm -f /tmp/pull-secret-*.yaml" EXIT
+
     echo "****** Logging into private registry..."
     echo "${REGISTRY_PASSWORD}" | docker login ${REGISTRY_NAME} -u "${REGISTRY_USER}" --password-stdin
+
     echo "****** Creating pull secret..."
     oc create secret docker-registry regcred --docker-server=${REGISTRY_NAME} "--docker-username=${REGISTRY_USER}" "--docker-password=${REGISTRY_PASSWORD}" --docker-email=unused
+
+    oc get secret/regcred -o jsonpath='{.data.\.dockerconfigjson}' | base64 --decode > /tmp/pull-secret-new.yaml
+    oc get secret/pull-secret -n openshift-config -o jsonpath='{.data.\.dockerconfigjson}' | base64 --decode > /tmp/pull-secret-global.yaml
+
+    jq -s '.[1] * .[0]' /tmp/pull-secret-new.yaml /tmp/pull-secret-global.yaml > /tmp/pull-secret-merged.yaml
+
+    echo "Updating global pull secret"
+    oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=/tmp/pull-secret-merged.yaml
+
     echo "****** Installing bundle..."
     operator-sdk run bundle --install-mode OwnNamespace --pull-secret-name regcred "${BUNDLE_IMAGE}" || {
         echo "****** Installing bundle failed..."
@@ -107,12 +135,12 @@ main() {
     }
 
     # Wait for operator deployment to be ready
-    while [[ $(oc get deploy olo-controller-manager -o jsonpath='{ .status.readyReplicas }') -ne "1" ]]; do
-        echo "****** Waiting for olo-controller-manager to be ready..."
+    while [[ $(oc get deploy "${CONTROLLER_MANAGER_NAME}" -o jsonpath='{ .status.readyReplicas }') -ne "1" ]]; do
+        echo "****** Waiting for ${CONTROLLER_MANAGER_NAME} to be ready..."
         sleep 10
     done
 
-    echo "****** olo-controller-manager deployment is ready..."
+    echo "****** ${CONTROLLER_MANAGER_NAME} deployment is ready..."
 
     echo "****** Starting scorecard tests..."
     operator-sdk scorecard --verbose --kubeconfig  ${HOME}/.kube/config --selector=suite=kuttlsuite --namespace="${TEST_NAMESPACE}" --service-account="scorecard-kuttl" --wait-time 30m ./bundle || {
@@ -128,23 +156,23 @@ main() {
 }
 
 parse_args() {
-    while [ $# -gt 0 ]; do
+  while [ $# -gt 0 ]; do
     case "$1" in
     -u)
       shift
-      readonly USER="${1}"
+      readonly DOCKER_USERNAME="${1}"
       ;;
     -p)
       shift
-      readonly PASS="${1}"
+      readonly DOCKER_PASSWORD="${1}"
       ;;
     --cluster-url)
       shift
-      readonly OC_URL="${1}"
+      readonly CLUSTER_URL="${1}"
       ;;
     --cluster-token)
       shift
-      readonly OC_TOKEN="${1}"
+      readonly CLUSTER_TOKEN="${1}"
       ;;
     --registry-name)
       shift
@@ -161,7 +189,15 @@ parse_args() {
     --registry-password)
       shift
       readonly REGISTRY_PASSWORD="${1}"
-      ;;      
+      ;;
+    --release)
+      shift
+      readonly RELEASE="${1}"
+      ;;
+    --test-tag)
+      shift
+      readonly TEST_TAG="${1}"
+      ;;
     *)
       echo "Error: Invalid argument - $1"
       echo "$usage"
