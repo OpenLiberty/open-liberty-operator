4cfb724446ef3e37685b3b37d1343e3c1cd35dc2
leochr <leojc@ca.ibm.com>
2021-12-17 11:44:18 -0500

    Add description for dump file path
    Signed-off-by: leochr <leojc@ca.ibm.com>

--- api/v1beta2/openlibertydump_types.go
+++ api/v1beta2/openlibertydump_types.go
@@ -32,6 +32,7 @@ const (
 type OpenLibertyDumpStatus struct {
 	// +listType=atomic
 	Conditions []OperationStatusCondition `json:"conditions,omitempty"`
+	// Location of the generated dump file
 	// +operator-sdk:csv:customresourcedefinitions:type=status,displayName="Dump File Path",xDescriptors="urn:alm:descriptor:com.tectonic.ui:text"
 	DumpFile string `json:"dumpFile,omitempty"`
 }
