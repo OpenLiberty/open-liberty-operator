0c2c171d4ea38df7c4612ece4fd44c923e61904e
ilewis <idlewis@users.noreply.github.com>
2022-06-20 11:59:16 +0100

    More changes for minikube tests on 0.8.2

--- scripts/e2e-minikube.sh
+++ scripts/e2e-minikube.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 readonly usage="Usage: e2e-minikube.sh --test-tag <test-id>"
-readonly SERVICE_ACCOUNT="travis-tests"
+
+readonly LOCAL_REGISTRY="localhost:5000"
+readonly BUILD_IMAGE="open-liberty-operator:latest"
+readonly DAILY_IMAGE="openliberty\/operator:daily"
+
+readonly RUNASUSER="\n  securityContext:\n    runAsUser: 1001"
+readonly APPIMAGE='applicationImage:\s'
+readonly IMAGES=('k8s.gcr.io\/pause:2.0' 'navidsh\/demo-day')
+
 
 # setup_env: Download kubectl cli and Minikube, start Minikube, and create a test project
 setup_env() {
@@ -22,25 +30,35 @@ setup_env() {
     kubectl label node "minikube" kuttlTest=test1
 }
 
-# install_olo: Kustomize and install Open-Liberty-Operator
+build_push() {
+    eval "$(minikube docker-env --profile=minikube)" && export DOCKER_CLI='docker'
+    ## Build Docker image and push to local registry
+    docker build -t "${LOCAL_REGISTRY}/${BUILD_IMAGE}" .
+    docker push "${LOCAL_REGISTRY}/${BUILD_IMAGE}"
+}
+
+# install_olo: Kustomize and install Runtime-Component-Operator
 install_olo() {
     echo "****** Installing OLO in namespace: ${TEST_NAMESPACE}"
     kubectl apply -f bundle/manifests/apps.openliberty.io_openlibertyapplications.yaml
     kubectl apply -f bundle/manifests/apps.openliberty.io_openlibertydumps.yaml
     kubectl apply -f bundle/manifests/apps.openliberty.io_openlibertytraces.yaml
-    
+
+    sed -i "s/image: ${DAILY_IMAGE}/image: ${LOCAL_REGISTRY}\/${BUILD_IMAGE}/" deploy/kustomize/daily/base/open-liberty-operator.yaml
     kubectl apply -f deploy/kustomize/daily/base/open-liberty-operator.yaml -n ${TEST_NAMESPACE}
 }
 
-function install_tools() {
+install_tools() {
     echo "****** Installing Prometheus"
     kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/bundle.yaml
-    
+
     echo "****** Installing Knative"
-    minikube addons enable registry
-    kubectl apply -f https://github.com/knative/serving/releases/download/v0.24.0/serving-crds.yaml
-    kubectl apply -f https://github.com/knative/eventing/releases/download/v0.24.0/eventing-crds.yaml
-  
+    kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.3.0/serving-crds.yaml
+    kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.3.0/eventing-crds.yaml
+
+    echo "****** Installing Cert Manager"
+    kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.yaml
+
     echo "****** Enabling Ingress"
     minikube addons enable ingress
 }
@@ -51,7 +69,7 @@ cleanup_env() {
     minikube stop && minikube delete
 }
 
-function setup_test() {
+setup_test() {
     echo "****** Installing kuttl"
     mkdir krew && cd krew
     curl -OL https://github.com/kubernetes-sigs/krew/releases/latest/download/krew-linux_amd64.tar.gz \
@@ -62,23 +80,34 @@ function setup_test() {
     kubectl krew install kuttl
 
     ## Add tests for minikube
-    mv bundle/tests/scorecard/minikube-kuttl/ingress bundle/tests/scorecard/kuttl/
-    mv bundle/tests/scorecard/minikube-kuttl/ingress-certificate bundle/tests/scorecard/kuttl/
+    # mv bundle/tests/scorecard/minikube-kuttl/ingress bundle/tests/scorecard/kuttl/
+    # mv bundle/tests/scorecard/minikube-kuttl/ingress-certificate bundle/tests/scorecard/kuttl/
     
     ## Remove tests that do not apply for minikube
-    mv bundle/tests/scorecard/kuttl/routes bundle/tests/scorecard/minikube-kuttl/
+    mv bundle/tests/scorecard/kuttl/routes            bundle/tests/scorecard/minikube-kuttl/
     mv bundle/tests/scorecard/kuttl/route-certificate bundle/tests/scorecard/minikube-kuttl/
-    mv bundle/tests/scorecard/kuttl/image-stream bundle/tests/scorecard/minikube-kuttl/
+    mv bundle/tests/scorecard/kuttl/image-stream      bundle/tests/scorecard/minikube-kuttl/
+
+    for image in "${IMAGES[@]}"
+    do 
+        files=($(grep -rwl 'bundle/tests/scorecard/kuttl/' -e $APPIMAGE$image))
+        for file in "${files[@]}"
+        do
+            sed -i "s/$image/$image$RUNASUSER/" $file
+        done
+    done
 }
 
-function cleanup_test() {
+cleanup_test() {
     ## Restore tests
-    mv bundle/tests/scorecard/kuttl/ingress bundle/tests/scorecard/minikube-kuttl/
-    mv bundle/tests/scorecard/kuttl/ingress-certificate bundle/tests/scorecard/minikube-kuttl/
+    # mv bundle/tests/scorecard/kuttl/ingress bundle/tests/scorecard/minikube-kuttl/
+    # mv bundle/tests/scorecard/kuttl/ingress-certificate bundle/tests/scorecard/minikube-kuttl/
     
-    mv bundle/tests/scorecard/minikube-kuttl/routes bundle/tests/scorecard/kuttl/ 
+    mv bundle/tests/scorecard/minikube-kuttl/routes            bundle/tests/scorecard/kuttl/ 
     mv bundle/tests/scorecard/minikube-kuttl/route-certificate bundle/tests/scorecard/kuttl/ 
-    mv bundle/tests/scorecard/minikube-kuttl/image-stream bundle/tests/scorecard/kuttl/
+    mv bundle/tests/scorecard/minikube-kuttl/image-stream      bundle/tests/scorecard/kuttl/
+
+    git restore bundle/tests/scorecard deploy
 }
 
 main() {
@@ -92,6 +121,7 @@ main() {
 
     echo "****** Setting up test environment..."
     setup_env
+    build_push
     install_olo
     install_tools
 
@@ -107,13 +137,15 @@ main() {
     echo "****** Starting minikube scorecard tests..."
     operator-sdk scorecard --verbose --selector=suite=kuttlsuite --namespace "${TEST_NAMESPACE}" --service-account scorecard-kuttl --wait-time 30m ./bundle || {
         echo "****** Scorecard tests failed..."
+        echo "****** Cleaning up test environment..."
+        cleanup_test
+        cleanup_env
         exit 1
     }
     result=$?
-    
-    cleanup_test
 
     echo "****** Cleaning up test environment..."
+    cleanup_test
     cleanup_env
 
     return $result
