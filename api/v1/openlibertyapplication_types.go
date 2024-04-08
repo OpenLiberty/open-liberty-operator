19aef7daf9ab4138cf3cf104c8d16cb83f855070
Kirby Chin <kirbychin99@gmail.com>
2024-04-08 11:44:39 -0400

    Update observedGeneration description

--- api/v1/openlibertyapplication_types.go
+++ api/v1/openlibertyapplication_types.go
@@ -433,7 +433,7 @@ type OpenLibertyApplicationStatus struct {
 
 	SemeruCompiler *SemeruCompilerStatus `json:"semeruCompiler,omitempty"`
 
-	// The last observed generation of this OpenLibertyApplication instance reconciled by the operator.
+	// The last generation of this OpenLibertyApplication instance observed by the operator.
 	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
 }
 
