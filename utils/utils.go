4735934b491ede7c6e20bcb0cb5eb16be9dc916b
Leo Christy Jesuraj <leojc@ca.ibm.com>
2024-08-28 16:47:43 -0400

    Update operator version to 1.4.0 and channel to v1.4 (#614)
    Signed-off-by: Leo Christy Jesuraj <leojc@ca.ibm.com>

--- utils/utils.go
+++ utils/utils.go
@@ -34,7 +34,7 @@ var log = logf.Log.WithName("openliberty_utils")
 // Constant Values
 const serviceabilityMountPath = "/serviceability"
 const ssoEnvVarPrefix = "SEC_SSO_"
-const OperandVersion = "1.3.3"
+const OperandVersion = "1.4.0"
 const ltpaKeysMountPath = "/config/managedLTPA"
 const ltpaServerXMLOverridesMountPath = "/config/configDropins/overrides/"
 const LTPAServerXMLSuffix = "-managed-ltpa-server-xml"
