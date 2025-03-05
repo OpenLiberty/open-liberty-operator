cdfc4e61bc5f3405cb1045121525ba27dc93edcc
kabicin <37311900+kabicin@users.noreply.github.com>
2025-03-05 14:58:51 -0500

    Update trace unit tests

--- internal/controller/trace_sharing.go
+++ internal/controller/trace_sharing.go
@@ -7,7 +7,6 @@ import (
 	olv1 "github.com/OpenLiberty/open-liberty-operator/api/v1"
 	lutils "github.com/OpenLiberty/open-liberty-operator/utils"
 	tree "github.com/OpenLiberty/open-liberty-operator/utils/tree"
-	corev1 "k8s.io/api/core/v1"
 )
 
 const TRACE_RESOURCE_SHARING_FILE_NAME = "trace"
@@ -30,6 +29,7 @@ func (r *ReconcileOpenLibertyTrace) reconcileTraceMetadata(instance *olv1.OpenLi
 		if err != nil {
 			return metadataList, err
 		}
+
 		// validate that the decision path such as "v1_4_0.managePasswordEncryption.<pathChoices[n-1]>" is a valid subpath in treeMap
 		// an error here indicates a build time error created by the operator developer or pollution of the ltpa-decision-tree.yaml
 		// Note: validSubPath is a substring of labelString and a valid path within treeMap; it will always hold that len(validSubPath) <= len(labelString)
@@ -49,6 +49,7 @@ func (r *ReconcileOpenLibertyTrace) reconcileTraceMetadata(instance *olv1.OpenLi
 		if err != nil {
 			return metadataList, err
 		}
+
 		// if the leaderTracker is on a mismatched version, wait for a subsequent reconcile loop to re-create the leader tracker
 		if leaderTracker.Labels[lutils.LeaderVersionLabel] != latestOperandVersion {
 			return metadataList, fmt.Errorf("waiting for the Leader Tracker to be updated")
@@ -57,18 +58,19 @@ func (r *ReconcileOpenLibertyTrace) reconcileTraceMetadata(instance *olv1.OpenLi
 		// to avoid limitation with Kubernetes label values having a max length of 63, translate validSubPath into a path index
 		pathIndex := tree.GetLeafIndex(treeMap, validSubPath)
 		versionedPathIndex := fmt.Sprintf("%s.%d", latestOperandVersion, pathIndex)
-
-		metadata.Path = metadataPath
-		metadata.PathIndex = versionedPathIndex
-		metadata.Name = r.getTraceMetadataName(instance, leaderTracker, metadataPath, assetsFolder)
-		metadataList.Items = append(metadataList.Items, metadata)
+		if n := len(pathChoices); n > 0 {
+			metadata.Path = metadataPath
+			metadata.PathIndex = versionedPathIndex
+			metadata.Name = pathChoices[n-1] // at least for v1_4_2, pathChoices[n-1] will be the wildcard entry * representing the name
+			metadataList.Items = append(metadataList.Items, metadata)
+		}
 	}
 	return metadataList, nil
 }
 
 func (r *ReconcileOpenLibertyTrace) getTracePathOptionsAndChoices(instance *olv1.OpenLibertyTrace, latestOperandVersion string) ([][]string, [][]string) {
 	var pathOptionsList, pathChoicesList [][]string
-	if latestOperandVersion == "v1_4_1" {
+	if latestOperandVersion == "v1_4_2" {
 		// Generate a path option/choice for a leader to manage the Trace CR instance's pods
 		pathOptions := []string{"name"}                // ordering matters, it must follow the nodes of the Trace decision tree in trace-decision-tree.yaml
 		pathChoices := []string{instance.Spec.PodName} // wildcard entry can be provided as any string
@@ -84,9 +86,4 @@ func (r *ReconcileOpenLibertyTrace) getTracePathOptionsAndChoices(instance *olv1
 		}
 	}
 	return pathOptionsList, pathChoicesList
-}
-
-func (r *ReconcileOpenLibertyTrace) getTraceMetadataName(instance *olv1.OpenLibertyTrace, leaderTracker *corev1.Secret, validSubPath string, assetsFolder *string) string {
-	// the trace metadata name is always the instance's pod name
-	return instance.Spec.PodName
 }
