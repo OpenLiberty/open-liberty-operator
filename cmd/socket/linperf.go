b24056f3a76b5a3fdc6bc83b9ac2a9376eac19c4
kabicin <37311900+kabicin@users.noreply.github.com>
2025-07-18 10:46:19 -0400

    Add socket for reconciler to pod exec cmds via IPC

---
+++ cmd/socket/linperf.go
@@ -0,0 +1,92 @@
+package socket
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path"
+
+	_ "unsafe"
+
+	"github.com/OpenLiberty/open-liberty-operator/utils"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/remotecommand"
+	"k8s.io/kubectl/pkg/scheme"
+)
+
+//go:linkname cpMakeTar k8s.io/kubectl/pkg/cmd/cp.makeTar
+func cpMakeTar(srcPath, destPath string, writer io.Writer) error
+
+func CopyAndRunLinperf(restConfig *rest.Config, podName string, encodedAttr string, doneCallback func(error)) (*io.PipeReader, *io.PipeWriter, context.CancelFunc, error) {
+	containerName := "app"
+	sourceFolder := "internal/controller/assets/helper"
+	destFolder := "/output/helper"
+	linperfCmd := utils.GetLinperfCmd(encodedAttr, podName, "olo-test")
+	return CopyFolderToPodAndRunScript(restConfig, sourceFolder, destFolder, podName, "olo-test", containerName, linperfCmd, doneCallback)
+}
+
+func podExec(clientset *kubernetes.Clientset, podName, podNamespace, containerName string, usingStdin bool, command []string) *rest.Request {
+	return clientset.CoreV1().RESTClient().Post().
+		Resource("pods").
+		Name(podName).
+		Namespace(podNamespace).
+		SubResource("exec").
+		VersionedParams(&corev1.PodExecOptions{
+			Command:   command,
+			Container: containerName,
+			Stdin:     usingStdin,
+			Stdout:    true,
+			Stderr:    true,
+			TTY:       false,
+		}, scheme.ParameterCodec)
+}
+
+func CopyFolderToPodAndRunScript(config *rest.Config, srcFolder string, destFolder string, podName, podNamespace, containerName, scriptCmd string, doneCallback func(error)) (*io.PipeReader, *io.PipeWriter, context.CancelFunc, error) {
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("Failed to create Clientset: %v", err.Error())
+	}
+
+	reader, writer := io.Pipe()
+	go func() {
+		defer writer.Close()
+		cpMakeTar(srcFolder, destFolder, writer)
+	}()
+
+	command := []string{"tar", "-xf", "-"}
+	destDir := path.Dir(destFolder)
+	if len(destDir) > 0 {
+		command = append(command, "-C", destDir)
+	}
+
+	streamContext, cancelStreamContext := context.WithCancel(context.TODO())
+	go func() {
+		usingStdin := true
+		exec, err := remotecommand.NewSPDYExecutor(config, "POST", podExec(clientset, podName, podNamespace, containerName, usingStdin, command).URL())
+		if err != nil {
+			fmt.Printf("error %s\n", err)
+			doneCallback(err)
+		}
+		err = exec.StreamWithContext(streamContext, remotecommand.StreamOptions{
+			Stdin:  reader,
+			Stdout: os.Stdout,
+			Stderr: os.Stderr,
+			Tty:    false,
+		})
+
+		usingStdin = false
+		exec, err = remotecommand.NewSPDYExecutor(config, "POST", podExec(clientset, podName, podNamespace, containerName, usingStdin, []string{"/bin/sh", "-c", scriptCmd}).URL())
+		var stdout, stderr bytes.Buffer
+		err = exec.StreamWithContext(context.Background(), remotecommand.StreamOptions{
+			Stdout: &stdout,
+			Stderr: &stderr,
+			Tty:    false,
+		})
+		doneCallback(err)
+	}()
+	return reader, writer, cancelStreamContext, nil
+}
