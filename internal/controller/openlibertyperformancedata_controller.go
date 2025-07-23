7a0d31e4150f2ed5e38022dd27aed2e282d71a3a
kabicin <37311900+kabicin@users.noreply.github.com>
2025-07-23 12:15:22 -0400

    Allow operator to ping Liberty pod in deny-all env

--- internal/controller/openlibertyperformancedata_controller.go
+++ internal/controller/openlibertyperformancedata_controller.go
@@ -14,6 +14,7 @@ import (
 
 	openlibertyv1 "github.com/OpenLiberty/open-liberty-operator/api/v1"
 	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
