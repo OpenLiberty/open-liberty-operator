c36549571a3c6c2165d23f2a422f0421b2ced25a
kabicin <37311900+kabicin@users.noreply.github.com>
2025-08-12 09:19:09 -0400

    Update linperf.go

--- utils/socket/linperf.go
+++ utils/socket/linperf.go
@@ -80,7 +80,7 @@ func CopyFolderToPodAndRunScript(config *rest.Config, srcFolder string, destFold
 		command = append(command, "-C", destDir)
 	}
 
-	streamContext, cancelStreamContext := context.WithCancel(context.TODO())
+	streamContext, cancelStreamContext := context.WithCancel(context.Background())
 	go func() {
 		usingStdin := true
 		exec, err := remotecommand.NewSPDYExecutor(config, "POST", podExec(clientset, podName, podNamespace, containerName, usingStdin, command).URL())
