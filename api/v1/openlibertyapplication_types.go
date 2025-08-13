36c0fc1f145706a2402d70a838f84984c55a0f20
miuponn <kellygao@live.ca>
2025-08-13 14:41:12 -0400

    Added getter methods for SessionAffinity

--- api/v1/openlibertyapplication_types.go
+++ api/v1/openlibertyapplication_types.go
@@ -1617,3 +1617,10 @@ func convertFromCommonStatusEndpointScope(c common.StatusEndpointScope) StatusEn
 		panic(c)
 	}
 }
+
+func (s *OpenLibertyApplicationService) GetSessionAffinity() common.BaseComponentServiceSessionAffinity {
+    if s.SessionAffinity == nil {
+        return nil
+    }
+    return s.SessionAffinity
+}
