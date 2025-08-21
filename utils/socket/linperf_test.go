ea877f224db86ec652db92ab5a7745a721c177d3
kabicin <37311900+kabicin@users.noreply.github.com>
2025-08-21 12:02:34 -0400

    Customize linperf file name based on CR

--- utils/socket/linperf_test.go
+++ utils/socket/linperf_test.go
@@ -3,10 +3,10 @@ package socket
 import "testing"
 
 func Test_getLinperfDataFileName(t *testing.T) {
-	outputFile := getLinperfDataFileName(`2025-07-23 13:34:54 \tTemporary directory linperf_RESULTS.20250723.133044 removed.
+	outputFile := getLinperfDataFileName(`2025-07-23 13:34:54 \tTemporary directory linperf_RESULTS_sample.20250723.133044 removed.
 2025-07-23 13:34:54 \tlinperf script complete.
 To share with IBM support, upload all the following files:
-* /serviceability/olo-test/example-75dfd65979-mwvnz/performanceData/linperf_RESULTS.20250723.133044.tar.gz
+* /serviceability/olo-test/example-75dfd65979-mwvnz/performanceData/linperf_RESULTS_sample.20250723.133044.tar.gz
 * /var/log/messages (Linux OS files)
 For WebSphere Application Server:
 * Logs (systemout.log, native_stderr.log, etc)
@@ -15,7 +15,7 @@ For Liberty:
 * Logs (messages.log, console.log, etc)
 * server.env, server.xml, and jvm.options`)
 	tests := []Test{
-		{"capture output file", "/serviceability/olo-test/example-75dfd65979-mwvnz/performanceData/linperf_RESULTS.20250723.133044.tar.gz", outputFile},
+		{"capture output file", "/serviceability/olo-test/example-75dfd65979-mwvnz/performanceData/linperf_RESULTS_sample.20250723.133044.tar.gz", outputFile},
 	}
 	if err := verifyTests(tests); err != nil {
 		t.Fatalf("%v", err)
