b03bfd05e3e0dc6c91141700ccca1821151b7714
Kirby Chin <37311900+kabicin@users.noreply.github.com>
2026-03-04 16:43:11 -0500

    Use AddStatusWarning to better track existing status warnings (#838)
    * Dynamically update status warnings state based on reconcile status and prevent more than one status warning message
    
    * Pull RCO@warning-status changes
    
    * Add better warning message when failing to encode the aes key
    
    * Pull RCO@main changes
    
    * Remove errors.Wrapf call

--- internal/controller/ltpa_keys_sharing.go
+++ internal/controller/ltpa_keys_sharing.go
@@ -13,7 +13,6 @@ import (
 	lutils "github.com/OpenLiberty/open-liberty-operator/utils"
 	tree "github.com/OpenLiberty/open-liberty-operator/utils/tree"
 	"github.com/application-stacks/runtime-component-operator/common"
-	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -453,7 +452,13 @@ func (r *ReconcileOpenLiberty) generateLTPAConfig(instance *olv1.OpenLibertyAppl
 
 			encodedPassword, err := encode(password, currentPasswordEncryptionKey, currentAESEncryptionKey, common.LoadFromConfig(common.Config, lutils.OpConfigPasswordEncodingType))
 			if err != nil {
-				return "", errors.Wrapf(err, "failed to encode the password encryption key")
+				var encodeErrorMessage string
+				if usingAES {
+					encodeErrorMessage = "failed to encode using the aes encryption key, verify the provided key in Secret 'wlp-aes-encryption-key' is a valid base64 encoded AES-256 key"
+				} else {
+					encodeErrorMessage = "failed to encode using the password encryption key"
+				}
+				return "", fmt.Errorf("%s: %+v", encodeErrorMessage, err)
 			}
 
 			ltpaConfigSecret.Labels[lutils.ResourcePathIndexLabel] = ltpaConfigMetadata.PathIndex
